a4c99a4a0a5b001e0879478fc18cbcdcdd16230b
mctcooray <mct.buisness@gmail.com>
2026-02-14 20:16:06 +0530

    Implement service worker for offline caching

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
+const CACHE = "equip-inventory-offline-v1";
+const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((resp) => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return resp;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
+});
